ea7cc59429e65dbe0a51f8540bbd2d786b081f9d
Romain-P <skryn01@gmail.com>
2014-08-11 01:12:28 +0200

    pretty print guice exceptions

--- world/world-distrib/src/main/java/org/heat/world/App.java
+++ world/world-distrib/src/main/java/org/heat/world/App.java
@@ -1,11 +1,24 @@
 package org.heat.world;
 
+import com.google.inject.CreationException;
+import com.google.inject.ProvisionException;
+import com.google.inject.spi.Message;
 import org.rocket.dist.RocketLauncher;
 
 public final class App {
     private App() {}
 
     public static void main(String[] args) {
-        RocketLauncher.takeOff(new StdWorldRocket());
+        try {
+            RocketLauncher.run(new StdWorldRocket().getServiceContext());
+        } catch (CreationException e) {
+            for (Message message : e.getErrorMessages()) {
+                message.getCause().printStackTrace();
+            }
+        } catch (ProvisionException e) {
+            for (Message message : e.getErrorMessages()) {
+                message.getCause().printStackTrace();
+            }
+        }
     }
 }
