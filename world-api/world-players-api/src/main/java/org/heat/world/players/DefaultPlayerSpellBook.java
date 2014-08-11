6788e0a1cde5b592f87cacbeeb0580e9e266d695
Romain-P <skryn01@gmail.com>
2014-08-11 16:23:56 +0200

    default implementation of PlayerSpellBook

--- world-api/world-players-api/src/main/java/org/heat/world/players/DefaultPlayerSpellBook.java
+++ world-api/world-players-api/src/main/java/org/heat/world/players/DefaultPlayerSpellBook.java
@@ -1,12 +1,37 @@
 package org.heat.world.players;
 
+import com.ankamagames.dofus.datacenter.breeds.Breed;
+import com.ankamagames.dofus.datacenter.spells.Spell;
+import org.heat.data.Datacenter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalInt;
 import java.util.stream.Stream;
 
 public final class DefaultPlayerSpellBook implements PlayerSpellBook {
 
+    private final List<PlayerSpell> spells;
+
+    private DefaultPlayerSpellBook(List<PlayerSpell> spells) {
+        this.spells = spells;
+    }
+
+    public static DefaultPlayerSpellBook create(List<PlayerSpell> spells) {
+        return new DefaultPlayerSpellBook(new ArrayList<>(spells));
+    }
+
+    public static DefaultPlayerSpellBook create(Datacenter datacenter, Breed breed, int actualLevel) {
+        return new DefaultPlayerSpellBook(Players.buildDefaultBreedSpells(datacenter, breed, actualLevel));
+    }
+
+    @Override
+    public void add(Spell spell, OptionalInt position) {
+        spells.add(PlayerSpell.create(spell, Players.NEW_SPELL_DEFAULT_MIN_LEVEL, position));
+    }
 
     @Override
     public Stream<PlayerSpell> getSpellStream() {
-        return null;
+        return spells.stream();
     }
 }
