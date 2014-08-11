240c9c6a4c06f3fe8c965257bb022d3d7f143099
Romain-P <skryn01@gmail.com>
2014-08-11 16:23:41 +0200

    create PlayerSpellBook#add(Spell, OptionalInt)

--- world-api/world-players-api/src/main/java/org/heat/world/players/PlayerSpellBook.java
+++ world-api/world-players-api/src/main/java/org/heat/world/players/PlayerSpellBook.java
@@ -1,9 +1,14 @@
 package org.heat.world.players;
 
+import com.ankamagames.dofus.datacenter.spells.Spell;
+
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.stream.Stream;
 
 public interface PlayerSpellBook {
+    void add(Spell spell, OptionalInt position);
+
     Stream<PlayerSpell> getSpellStream();
 
     default Optional<PlayerSpell> findById(int id) {
