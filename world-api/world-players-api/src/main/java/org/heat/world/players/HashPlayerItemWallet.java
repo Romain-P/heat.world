a572b69d13ec1bb60e10e64497e9959f6f799b67
Romain-P <skryn01@gmail.com>
2014-08-13 01:52:11 +0200

    an amount of kamas is always positive or zero

--- world-api/world-players-api/src/main/java/org/heat/world/players/HashPlayerItemWallet.java
+++ world-api/world-players-api/src/main/java/org/heat/world/players/HashPlayerItemWallet.java
@@ -1,9 +1,16 @@
 package org.heat.world.players;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.heat.world.items.HashItemBag;
 
 public final class HashPlayerItemWallet extends HashItemBag implements PlayerItemWallet {
-    @Getter @Setter int kamas;
+    @Getter int kamas;
+
+    @Override
+    public void setKamas(int kamas) {
+        if (kamas < 0) {
+            throw new IllegalArgumentException("an amount of kamas is always positive or zero");
+        }
+        this.kamas = kamas;
+    }
 }
