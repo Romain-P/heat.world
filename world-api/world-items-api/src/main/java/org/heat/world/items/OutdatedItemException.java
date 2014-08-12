38ce37135fa0599d4c5412ff49fe3d7b1e6d2ea3
Romain-P <skryn01@gmail.com>
2014-08-12 19:58:08 +0200

    automatically determine outdated item

--- world-api/world-items-api/src/main/java/org/heat/world/items/OutdatedItemException.java
+++ world-api/world-items-api/src/main/java/org/heat/world/items/OutdatedItemException.java
@@ -4,10 +4,15 @@ public class OutdatedItemException extends RuntimeException {
     private final WorldItem outdated;
     private final WorldItem updated;
 
-    public OutdatedItemException(WorldItem outdated, WorldItem updated) {
+    public OutdatedItemException(WorldItem left, WorldItem right) {
         super("an item is outdated");
-        this.outdated = outdated;
-        this.updated = updated;
+        if (left.getVersion() < right.getVersion()) {
+            this.outdated = left;
+            this.updated = right;
+        } else {
+            this.outdated = right;
+            this.updated = left;
+        }
     }
 
     public WorldItem getOutdated() {
