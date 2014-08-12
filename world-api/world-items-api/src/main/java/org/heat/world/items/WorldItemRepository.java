035dec5179ddef90429f1c049d325a564ffdbc71
Romain-P <skryn01@gmail.com>
2014-08-12 19:58:38 +0200

    full WorldItemRepository async

--- world-api/world-items-api/src/main/java/org/heat/world/items/WorldItemRepository.java
+++ world-api/world-items-api/src/main/java/org/heat/world/items/WorldItemRepository.java
@@ -2,13 +2,12 @@ package org.heat.world.items;
 
 import org.fungsi.concurrent.Future;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public interface WorldItemRepository {
-    Optional<WorldItem> find(int uid);
-    Stream<WorldItem> find(IntStream uids);
+    Future<WorldItem> find(int uid);
+    Future<List<WorldItem>> find(IntStream uids);
 
     Future<WorldItem> save(WorldItem item);
     Future<WorldItem> remove(WorldItem item);
