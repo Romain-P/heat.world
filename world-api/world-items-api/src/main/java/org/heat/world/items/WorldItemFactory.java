097c04d0e46ab9f2988a8e131b6bc830f0dafadc
Romain-P <skryn01@gmail.com>
2014-08-12 22:53:41 +0200

    create new items with a template id rather than a raw template

--- world-api/world-items-api/src/main/java/org/heat/world/items/WorldItemFactory.java
+++ world-api/world-items-api/src/main/java/org/heat/world/items/WorldItemFactory.java
@@ -1,13 +1,11 @@
 package org.heat.world.items;
 
-import com.ankamagames.dofus.datacenter.items.Item;
-
 public interface WorldItemFactory {
     /**
-     * Create an item given a template and a quantity
-     * @param template a non-null item
+     * Create an item given a template id and a quantity
+     * @param templateId an integer
      * @param quantity an integer
      * @return a non-null item
      */
-    WorldItem create(Item template, int quantity);
+    WorldItem create(int templateId, int quantity);
 }
