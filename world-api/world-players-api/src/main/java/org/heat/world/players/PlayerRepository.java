646885682dbfade21cc4b3b58164eff72322c7ff
Romain-P <skryn01@gmail.com>
2014-08-18 19:08:51 +0200

    linearize player repository

--- world-api/world-players-api/src/main/java/org/heat/world/players/PlayerRepository.java
+++ world-api/world-players-api/src/main/java/org/heat/world/players/PlayerRepository.java
@@ -1,17 +1,13 @@
 package org.heat.world.players;
 
-import org.heat.shared.database.MutableRepository;
-
 import java.util.List;
 import java.util.Optional;
 
-public interface PlayerRepository extends MutableRepository<Player> {
+public interface PlayerRepository {
     Optional<Player> find(int id);
     List<Player> findByUserId(int userId);
     Optional<Player> findByName(String name);
 
-    @Override
-    default Optional<Player> find(long id) {
-        return find((int) id);
-    }
+    void save(Player player);
+    void remove(Player player);
 }
