276f726b51fd9db5d83dd907b302d662a7e5f9d0
Romain-P <skryn01@gmail.com>
2014-08-18 18:52:35 +0200

    install standard items module

--- world/world-distrib/src/main/java/org/heat/world/StdWorldModule.java
+++ world/world-distrib/src/main/java/org/heat/world/StdWorldModule.java
@@ -26,6 +26,7 @@ public class StdWorldModule extends AbstractModule {
         install(new StdUsersModule());
         install(new StdPlayersModule());
         install(new StdWorldEnvironmentModule());
+        install(new StdItemsModule());
     }
 
     @Provides
