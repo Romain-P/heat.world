8dacdab6088906f0193a24175e6cf9fd2ada752e
Romain-P <skryn01@gmail.com>
2014-08-18 21:41:09 +0200

    fully asynchronous jdbc player items repository

--- world-api/world-players-api/src/main/java/org/heat/world/players/JdbcPlayerItemRepository.java
+++ world-api/world-players-api/src/main/java/org/heat/world/players/JdbcPlayerItemRepository.java
@@ -3,6 +3,7 @@ package org.heat.world.players;
 import org.fungsi.Unit;
 import org.fungsi.concurrent.Future;
 import org.fungsi.concurrent.Futures;
+import org.fungsi.concurrent.Worker;
 import org.heat.world.items.WorldItem;
 import org.heat.world.items.WorldItemRepository;
 
@@ -18,11 +19,13 @@ import java.util.stream.IntStream;
 public final class JdbcPlayerItemRepository implements PlayerItemRepository {
     private final DataSource dataSource;
     private final WorldItemRepository items;
+    private final Worker worker;
 
     @Inject
-    public JdbcPlayerItemRepository(DataSource dataSource, WorldItemRepository items) {
+    public JdbcPlayerItemRepository(DataSource dataSource, WorldItemRepository items, Worker worker) {
         this.dataSource = dataSource;
         this.items = items;
+        this.worker = worker;
     }
 
     private String batchInsertQuery(int rows) {
@@ -34,40 +37,45 @@ public final class JdbcPlayerItemRepository implements PlayerItemRepository {
         return builder.toString();
     }
 
+    private Future<IntStream> findItemUids(int playerId) {
+        return worker.submit(() -> {
+            try (Connection co = dataSource.getConnection()) {
+                try (PreparedStatement s = co.prepareStatement("select item_uid from player_items where player_id=?")) {
+                    s.setInt(1, playerId);
+
+                    IntStream.Builder uids = IntStream.builder();
+                    try (ResultSet rset = s.executeQuery()) {
+                        while (rset.next()) {
+                            int uid = rset.getInt(1);
+                            uids.add(uid);
+                        }
+                    }
+
+                    return uids.build();
+                }
+            } catch (SQLException e) {
+                throw new Error(e);
+            }
+        });
+    }
+
     @Override
     public Future<List<WorldItem>> findItemsByPlayer(int playerId) {
-        try (Connection co = dataSource.getConnection()) {
-            try (PreparedStatement s = co.prepareStatement("select item_uid from player_items where player_id=?")) {
-                s.setInt(1, playerId);
-
-                IntStream.Builder uids = IntStream.builder();
-                try (ResultSet rset = s.executeQuery()) {
-                    while (rset.next()) {
-                        int uid = rset.getInt(1);
-                        uids.add(uid);
-                    }
-                }
-
-                return items.find(uids.build());
-            }
-        } catch (SQLException e) {
-            throw new Error(e);
-        }
+        return findItemUids(playerId).flatMap(items::find);
     }
 
     @Override
     public Future<Unit> persist(int playerId, int itemId) {
-        try (Connection co = dataSource.getConnection()) {
-            try (PreparedStatement s = co.prepareStatement("insert into player_items(player_id, item_uid) values(?, ?)")) {
-                s.setInt(1, playerId);
-                s.setInt(2, itemId);
+        return worker.cast(() -> {
+            try (Connection co = dataSource.getConnection()) {
+                try (PreparedStatement s = co.prepareStatement("insert into player_items(player_id, item_uid) values(?, ?)")) {
+                    s.setInt(1, playerId);
+                    s.setInt(2, itemId);
 
-                s.executeUpdate();
-                return Futures.unit();
+                    s.executeUpdate();
+                }
             }
-        } catch (SQLException e) {
-            throw new Error(e);
-        }
+        });
     }
 
     @Override
@@ -78,33 +86,31 @@ public final class JdbcPlayerItemRepository implements PlayerItemRepository {
             return Futures.unit();
         }
 
-        try (Connection co = dataSource.getConnection()) {
-            try (PreparedStatement s = co.prepareStatement(batchInsertQuery(uids.length))) {
-                for (int i = 0, j = 1; i < uids.length; i++, j += 2) {
-                    s.setInt(j, playerId);
-                    s.setInt(j + 1, uids[i]);
-                }
+        return worker.cast(() -> {
+            try (Connection co = dataSource.getConnection()) {
+                try (PreparedStatement s = co.prepareStatement(batchInsertQuery(uids.length))) {
+                    for (int i = 0, j = 1; i < uids.length; i++, j += 2) {
+                        s.setInt(j, playerId);
+                        s.setInt(j + 1, uids[i]);
+                    }
 
-                s.execute();
-                return Futures.unit();
+                    s.execute();
+                }
             }
-        } catch (SQLException e) {
-            throw new Error(e);
-        }
+        });
     }
 
     @Override
     public Future<Unit> remove(int playerId, int itemId) {
-        try (Connection co = dataSource.getConnection()) {
-            try (PreparedStatement s = co.prepareStatement("delete from player_items where player_id=? and item_uid=?")) {
-                s.setInt(1, playerId);
-                s.setInt(2, itemId);
+        return worker.cast(() -> {
+            try (Connection co = dataSource.getConnection()) {
+                try (PreparedStatement s = co.prepareStatement("delete from player_items where player_id=? and item_uid=?")) {
+                    s.setInt(1, playerId);
+                    s.setInt(2, itemId);
 
-                s.executeUpdate();
-                return Futures.unit();
+                    s.executeUpdate();
+                }
             }
-        } catch (SQLException e) {
-            throw new Error(e);
-        }
+        });
     }
 }
