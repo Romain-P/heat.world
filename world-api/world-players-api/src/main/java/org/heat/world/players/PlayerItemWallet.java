14f967e655f2bd27f85c3f7c4db5a9ed506eb516
Romain-P <skryn01@gmail.com>
2014-08-18 18:43:33 +0200

    player's wallet has a weight, and a player has a maximum transportable weight

--- world-api/world-players-api/src/main/java/org/heat/world/players/PlayerItemWallet.java
+++ world-api/world-players-api/src/main/java/org/heat/world/players/PlayerItemWallet.java
@@ -3,4 +3,9 @@ package org.heat.world.players;
 import org.heat.world.items.WorldItemWallet;
 
 public interface PlayerItemWallet extends WorldItemWallet {
+    default int getWeight() {
+        return getItemStream()
+                .mapToInt(item -> (int) item.getTemplate().getRealWeight())
+                .reduce(0, Integer::sum);
+    }
 }
