163a88d98470c6ca88c6b96e50af94b345be2dbc
Romain-P <skryn01@gmail.com>
2014-08-18 18:28:37 +0200

    quick and dirty convertion

--- world-api/world-items-api/src/main/java/org/heat/world/items/Effects.java
+++ world-api/world-items-api/src/main/java/org/heat/world/items/Effects.java
@@ -1,10 +1,8 @@
 package org.heat.world.items;
 
 import com.ankamagames.dofus.datacenter.effects.EffectInstance;
-import com.ankamagames.dofus.datacenter.effects.instances.EffectInstanceDice;
-import com.ankamagames.dofus.datacenter.effects.instances.EffectInstanceInteger;
-import com.ankamagames.dofus.network.types.game.data.items.effects.ObjectEffect;
-import com.ankamagames.dofus.network.types.game.data.items.effects.ObjectEffectInteger;
+import com.ankamagames.dofus.datacenter.effects.instances.*;
+import com.ankamagames.dofus.network.types.game.data.items.effects.*;
 import org.heat.world.items.effects.FallbackItemEffect;
 import org.heat.world.items.effects.IntegerItemEffect;
 
@@ -43,6 +41,87 @@ final class Effects {
     }
 
     public static ObjectEffect toObjectEffect(EffectInstance effect) {
-        throw new UnsupportedOperationException("not implemented");
+        if (effect instanceof EffectInstanceLadder) {
+            return new ObjectEffectLadder(
+                    (short) effect.getEffectId(),
+                    (short) ((EffectInstanceLadder) effect).getMonsterFamilyId(),
+                    (short) ((EffectInstanceLadder) effect).getMonsterCount()
+            );
+        }
+
+        if (effect instanceof EffectInstanceCreature) {
+            return new ObjectEffectCreature(
+                    (short) effect.getEffectId(),
+                    (short) ((EffectInstanceCreature) effect).getMonsterFamilyId()
+            );
+        }
+
+        if (effect instanceof EffectInstanceDate) {
+            return new ObjectEffectDate(
+                    (short) effect.getEffectId(),
+                    (short) ((EffectInstanceDate) effect).getYear(),
+                    (short) ((EffectInstanceDate) effect).getMonth(),
+                    (short) ((EffectInstanceDate) effect).getDay(),
+                    (short) ((EffectInstanceDate) effect).getHour(),
+                    (short) ((EffectInstanceDate) effect).getMinute()
+            );
+        }
+
+        if (effect instanceof EffectInstanceDice) {
+            short num = (short) ((EffectInstanceDice) effect).getDiceNum();
+            short side = (short) ((EffectInstanceDice) effect).getDiceSide();
+            short constt = (short) 0;
+            if (side == 0) {
+                constt = num;
+                num = (short) 0;
+            }
+            return new ObjectEffectDice(
+                    (short) effect.getEffectId(),
+                    num,
+                    side,
+                    constt
+            );
+        }
+
+        if (effect instanceof EffectInstanceDuration) {
+            return new ObjectEffectDuration(
+                    (short) effect.getEffectId(),
+                    (short) ((EffectInstanceDuration) effect).getDays(),
+                    (short) ((EffectInstanceDuration) effect).getHours(),
+                    (short) ((EffectInstanceDuration) effect).getMinutes()
+            );
+        }
+        if (effect instanceof EffectInstanceInteger) {
+            return new ObjectEffectInteger(
+                    (short) effect.getEffectId(),
+                    (short) ((EffectInstanceInteger) effect).getValue()
+            );
+        }
+
+        if (effect instanceof EffectInstanceMinMax) {
+            return new ObjectEffectMinMax(
+                    (short) effect.getEffectId(),
+                    (short) ((EffectInstanceMinMax) effect).getMin(),
+                    (short) ((EffectInstanceMinMax) effect).getMax()
+            );
+        }
+
+        if (effect instanceof EffectInstanceMount) {
+            return new ObjectEffectMount(
+                    (short) effect.getEffectId(),
+                    (int) ((EffectInstanceMount) effect).getMountId(),
+                    ((EffectInstanceMount) effect).getDate(),
+                    (short) ((EffectInstanceMount) effect).getModelId()
+            );
+        }
+
+        if (effect instanceof EffectInstanceString) {
+            return new ObjectEffectString(
+                    (short) effect.getEffectId(),
+                    ((EffectInstanceString) effect).getText()
+            );
+        }
+
+        throw new IllegalArgumentException();
     }
 }
