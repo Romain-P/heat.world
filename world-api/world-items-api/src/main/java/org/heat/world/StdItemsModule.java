13fc2fdff78b5cc764556a820dc5d70d83e16b95
Romain-P <skryn01@gmail.com>
2014-08-22 01:43:27 +0200

    use new generation jdbc repository

--- world-api/world-items-api/src/main/java/org/heat/world/StdItemsModule.java
+++ world-api/world-items-api/src/main/java/org/heat/world/StdItemsModule.java
@@ -4,10 +4,12 @@ import com.ankamagames.dofus.network.ProtocolTypeManager;
 import com.ankamagames.dofus.network.types.game.data.items.effects.ObjectEffect;
 import com.google.inject.PrivateModule;
 import com.google.inject.Provides;
+import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 import org.fungsi.concurrent.Worker;
 import org.fungsi.concurrent.Workers;
 import org.heat.dofus.network.NetworkComponentFactory;
+import org.heat.shared.database.Table;
 import org.heat.world.items.*;
 
 import java.util.concurrent.ExecutorService;
@@ -15,6 +17,7 @@ import java.util.concurrent.ExecutorService;
 public class StdItemsModule extends PrivateModule {
     @Override
     protected void configure() {
+        bind(new TypeLiteral<Table<WorldItem>>() {}).to(WorldItemTable.class);
         bind(WorldItemFactory.class).to(DefaultItemFactory.class).asEagerSingleton();
         bind(WorldItemRepository.class).annotatedWith(Names.named("base")).to(JdbcItemRepository.class).asEagerSingleton();
         bind(WorldItemRepository.class).to(PermLazyItemRepository.class).asEagerSingleton();
--- world-api/world-items-api/src/main/java/org/heat/world/items/JdbcItemRepository.java
+++ world-api/world-items-api/src/main/java/org/heat/world/items/JdbcItemRepository.java
@@ -1,125 +1,69 @@
 package org.heat.world.items;
 
-import com.ankamagames.dofus.datacenter.items.Item;
-import com.ankamagames.dofus.network.enums.CharacterInventoryPositionEnum;
-import com.ankamagames.dofus.network.types.game.data.items.effects.ObjectEffect;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 import lombok.SneakyThrows;
 import org.fungsi.Throwables;
 import org.fungsi.concurrent.Future;
-import org.fungsi.concurrent.Futures;
 import org.fungsi.concurrent.Worker;
-import org.heat.data.Datacenter;
-import org.heat.dofus.network.NetworkComponentFactory;
-import org.heat.shared.database.JdbcRepository;
-import org.heat.shared.io.AutoGrowingWriter;
-import org.heat.shared.io.DataReader;
-import org.heat.shared.io.HeapDataReader;
-import org.heat.shared.stream.ImmutableCollectors;
-import org.heat.shared.stream.MoreCollectors;
+import org.heat.shared.database.JdbcRepositoryNG;
+import org.heat.shared.database.Table;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-public final class JdbcItemRepository extends JdbcRepository implements WorldItemRepository {
+@Singleton
+public final class JdbcItemRepository extends JdbcRepositoryNG<WorldItem> implements WorldItemRepository {
     private final DataSource dataSource;
-    private final Worker worker;
-    private final Datacenter datacenter;
-    private final NetworkComponentFactory<ObjectEffect> effectFactory;
 
-    private final AtomicInteger idGenerator = new AtomicInteger();
+    private final AtomicInteger nextUid;
 
     @Inject
-    public JdbcItemRepository(DataSource dataSource, Worker worker, Datacenter datacenter, NetworkComponentFactory<ObjectEffect> effectFactory) {
+    public JdbcItemRepository(Table<WorldItem> table, Worker worker, DataSource dataSource) {
+        super(table, worker);
         this.dataSource = dataSource;
-        this.worker = worker;
-        this.datacenter = datacenter;
-        this.effectFactory = effectFactory;
+        this.nextUid = new AtomicInteger(loadNextUid(dataSource));
     }
 
-    ImmutableList<String> columns = ImmutableList.of(
-            "uid",
-            "gid",
-            "effects",
-            "position",
-            "quantity"
-    );
-
-    ImmutableList<String> modifiableColumns = ImmutableList.of(
-            "effects",
-            "position",
-            "quantity"
-    );
+    String createSelectMultipleQuery(IntStream stream) {
+        return stream.mapToObj(uid -> "uid=" + uid)
+                .collect(Collectors.joining(" OR ", getSelectQuery() + " ", ""));
+    }
 
     @SneakyThrows
-    ImmutableSet<WorldItemEffect> importEffects(ResultSet rset) {
-        ImmutableSet.Builder<WorldItemEffect> effects = ImmutableSet.builder();
-
-        byte[] bytes = rset.getBytes("effects");
-        DataReader reader = new HeapDataReader(bytes, 0, bytes.length);
-        while (reader.canRead(2)) {
-            int typeId = reader.read_ui16();
-            ObjectEffect effect = effectFactory.create(typeId).get();
-            effect.deserialize(reader);
-
-            effects.add(Effects.fromObjectEffect(effect));
+    static int loadNextUid(DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("select max(uid) as uid from items"))
+        {
+            return resultSet.next()
+                    ? resultSet.getInt("uid")
+                    : 0;
+        } catch (SQLException e) {
+            throw Throwables.propagate(e);
         }
-
-        return effects.build();
     }
 
-    @SneakyThrows
-    void exportEffects(PreparedStatement s, int index, ImmutableSet<WorldItemEffect> effects) {
-        AutoGrowingWriter writer = new AutoGrowingWriter();
-        for (WorldItemEffect effect : effects) {
-            ObjectEffect e = effect.toObjectEffect();
-            writer.write_ui16(e.getProtocolId());
-            e.serialize(writer);
-        }
-        s.setBytes(index, writer.toByteArray()); // TODO(world/items): convert AutoGrowingWriter to InputStream
+    @Override
+    protected WorldItem pipelineInsert(WorldItem val) {
+        return val.withUid(nextUid.incrementAndGet());
     }
 
-    @SneakyThrows
-    WorldItem importFromDb(ResultSet rset) {
-        return WorldItem.create(
-                rset.getInt("uid"),
-                0,
-                datacenter.find(Item.class, rset.getInt("gid")).get(),
-                importEffects(rset),
-                CharacterInventoryPositionEnum.valueOf(rset.getByte("position")).get(),
-                rset.getInt("quantity")
-        );
+    @Override
+    protected WorldItem pipelineUpdate(WorldItem val) {
+        return val.withNewVersion();
     }
 
-    @SuppressWarnings("UnusedAssignment")
-    @SneakyThrows
-    void insertToDb(WorldItem item, PreparedStatement s) {
-        int index = 1;
-        s.setInt(index++, item.getUid());
-        s.setInt(index++, item.getTemplate().getId());
-        exportEffects(s, index++, item.getEffects());
-        s.setInt(index++, item.getPosition().value);
-        s.setInt(index++, item.getQuantity());
-    }
-
-    @SuppressWarnings("UnusedAssignment")
-    @SneakyThrows
-    void updateToDb(WorldItem item, PreparedStatement s) {
-        int index = 1;
-        exportEffects(s, index++, item.getEffects());
-        s.setInt(index++, item.getPosition().value);
-        s.setInt(index++, item.getQuantity());
-        s.setInt(index++, item.getUid());
+    @Override
+    protected WorldItem pipelineDelete(WorldItem val) {
+        return val.withUid(0);
     }
 
     @SneakyThrows
@@ -128,97 +72,27 @@ public final class JdbcItemRepository extends JdbcRepository implements WorldIte
         return dataSource.getConnection();
     }
 
-    Stream<WorldItem> query(String rest) {
-        return query(simpleSelect("items", columns) + " " + rest, s -> {
-        }, this::importFromDb);
-    }
-
-    String buildBatchQuery(int[] uids) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("WHERE uid=");
-        sb.append(uids[0]);
-        for (int i = 1; i < uids.length; i++) {
-            sb.append(" OR uid=").append(uids[i]);
-        }
-        return sb.toString();
-    }
-
-    void doExecute(WorldItem item, String query, BiConsumer<WorldItem, PreparedStatement> fn) {
-        try (Connection connection = dataSource.getConnection()) {
-            connection.setAutoCommit(false);
-            try (PreparedStatement s = connection.prepareStatement(query)) {
-                fn.accept(item, s);
-                s.execute();
-            } catch (SQLException e) {
-                connection.rollback();
-                throw e;
-            } finally {
-                connection.commit();
-                connection.setAutoCommit(true);
-            }
-        } catch (SQLException e) {
-            throw Throwables.propagate(e);
-        }
-    }
-
-    void doInsert(WorldItem item) {
-        doExecute(item, simpleInsert("items", columns), this::insertToDb);
-    }
-
-    void doUpdate(WorldItem item) {
-        doExecute(item, simpleUpdate("items", "uid", modifiableColumns), this::updateToDb);
-    }
-
     @Override
     public Future<WorldItem> find(int uid) {
-        return worker.submit(() -> {
-            try (Stream<WorldItem> stream = query("WHERE uid=" + uid)) {
-                return stream.collect(MoreCollectors.unique());
-            }
-        });
+        return findFirstByIntColumn("uid", uid);
     }
 
     @Override
-    public Future<List<WorldItem>> find(IntStream uids) {
-        int[] arr = uids.toArray();
-        if (arr.length <= 0) {
-            return Futures.success(ImmutableList.of());
-        } else if (arr.length == 1) {
-            return find(arr[0]).map(ImmutableList::of);
-        }
-
-        String query = buildBatchQuery(arr);
-
-        return worker.submit(() -> {
-            try (Stream<WorldItem> stream = query(query)) {
-                return stream.collect(ImmutableCollectors.toList());
-            }
-        });
+    public Future<List<WorldItem>> find(IntStream stream) {
+        // TODO(world/items): split too much uids into sub-queries
+        return findList(createSelectMultipleQuery(stream), s -> {});
     }
 
     @Override
     public Future<WorldItem> save(WorldItem item) {
-        if (item.getUid() == 0) {
-            return worker.submit(() -> {
-                WorldItem newItem = item.withUid(idGenerator.incrementAndGet());
-                doInsert(newItem);
-                return newItem;
-            });
-        } else {
-            return worker.submit(() -> {
-                WorldItem newItem = item.withNewVersion();
-                doUpdate(newItem);
-                return newItem;
-            });
-        }
+        return item.getUid() == 0
+                ? insert(item)
+                : update(item)
+                ;
     }
 
     @Override
     public Future<WorldItem> remove(WorldItem item) {
-        return worker.submit(() -> {
-            WorldItem newItem = item.withUid(0);
-            execute(simpleDelete("items", "uid"), s -> s.setInt(1, item.getUid()));
-            return newItem;
-        });
+        return delete(item);
     }
 }
