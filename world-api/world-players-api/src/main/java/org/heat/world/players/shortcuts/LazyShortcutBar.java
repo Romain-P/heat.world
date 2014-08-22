5b667c0afa696fd61fe74fa7675b57835e74f150
Romain-P <skryn01@gmail.com>
2014-08-22 16:26:32 +0200

    create PlayerShortcutBar and a lazy impl

---
+++ world-api/world-players-api/src/main/java/org/heat/world/players/shortcuts/LazyShortcutBar.java
@@ -0,0 +1,132 @@
+package org.heat.world.players.shortcuts;
+
+import com.ankamagames.dofus.network.enums.ShortcutBarEnum;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public final class LazyShortcutBar implements PlayerShortcutBar {
+    private final PlayerShortcutRepository repository;
+    private final int playerId;
+
+    private Multimap<ShortcutBarEnum, PlayerShortcut> shortcuts;
+
+    public LazyShortcutBar(PlayerShortcutRepository repository, int playerId) {
+        this.repository = repository;
+        this.playerId = playerId;
+    }
+
+    private ShortcutBarEnum getShortcutType(PlayerShortcut shortcut) {
+        return shortcut instanceof SpellShortcut
+            ? ShortcutBarEnum.SPELL_SHORTCUT_BAR
+            : ShortcutBarEnum.GENERAL_SHORTCUT_BAR;
+    }
+
+    private Multimap<ShortcutBarEnum, PlayerShortcut> load() {
+        // TODO(world/players): shortcut load timeout
+        List<PlayerShortcut> loaded = repository.findAll(playerId).get();
+
+        Multimap<ShortcutBarEnum, PlayerShortcut> shortcuts = HashMultimap.create();
+
+        for (PlayerShortcut shortcut : loaded) {
+            ShortcutBarEnum bar = getShortcutType(shortcut);
+
+            shortcuts.put(bar, shortcut);
+        }
+
+        return shortcuts;
+    }
+
+    private void requireLoaded() {
+        // might need synchronization
+        if (shortcuts == null) {
+            shortcuts = load();
+        }
+    }
+
+    private Stream<PlayerShortcut> getShortcutsOf0(ShortcutBarEnum bar) {
+        return shortcuts.get(bar).stream();
+    }
+
+    private void add0(ShortcutBarEnum bar, PlayerShortcut shortcut) {
+        shortcuts.put(bar, shortcut);
+        repository.create(shortcut);
+    }
+
+    private void remove0(ShortcutBarEnum bar, PlayerShortcut shortcut) {
+        shortcuts.remove(bar, shortcut);
+        repository.remove(shortcut);
+    }
+
+    private void swap0(ShortcutBarEnum bar, PlayerShortcut from, PlayerShortcut to) {
+        PlayerShortcut newFrom = from.withSlot(to.getSlot());
+        PlayerShortcut newTo = to.withSlot(from.getSlot());
+
+        remove0(bar, from);
+        remove0(bar, to);
+        add0(bar, newFrom);
+        add0(bar, newTo);
+    }
+
+    @Override
+    public Stream<PlayerShortcut> getShortcutsOf(ShortcutBarEnum bar) {
+        requireLoaded();
+        return getShortcutsOf0(bar);
+    }
+
+    @Override
+    public Optional<PlayerShortcut> findShortcut(ShortcutBarEnum bar, int slot) {
+        requireLoaded();
+        return getShortcutsOf0(bar).filter(x -> x.getSlot() == slot).findAny();
+    }
+
+    @Override
+    public boolean add(PlayerShortcut shortcut) {
+        requireLoaded();
+
+        ShortcutBarEnum bar = getShortcutType(shortcut);
+        boolean alreadyTaken = getShortcutsOf0(bar).anyMatch(x -> x.getSlot() == shortcut.getSlot());
+
+        if (!alreadyTaken) {
+            add0(bar, shortcut);
+        }
+
+        return alreadyTaken;
+    }
+
+    @Override
+    public boolean remove(ShortcutBarEnum bar, int slot) {
+        requireLoaded();
+
+        Optional<PlayerShortcut> option = findShortcut(bar, slot);
+        if (option.isPresent()) {
+            PlayerShortcut shortcut = option.get();
+            remove0(bar, shortcut);
+        }
+
+        return option.isPresent();
+    }
+
+    @Override
+    public boolean swap(ShortcutBarEnum bar, int fromSlot, int toSlot) {
+        requireLoaded();
+
+        Optional<PlayerShortcut> fromOption = findShortcut(bar, fromSlot);
+        if (!fromOption.isPresent()) {
+            return false;
+        }
+        PlayerShortcut from = fromOption.get();
+
+        Optional<PlayerShortcut> toOption = findShortcut(bar, toSlot);
+        if (!toOption.isPresent()) {
+            return false;
+        }
+        PlayerShortcut to = toOption.get();
+
+        swap0(bar, from, to);
+        return true;
+    }
+}
---
+++ world-api/world-players-api/src/main/java/org/heat/world/players/shortcuts/PlayerShortcutBar.java
@@ -0,0 +1,47 @@
+package org.heat.world.players.shortcuts;
+
+import com.ankamagames.dofus.network.enums.ShortcutBarEnum;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public interface PlayerShortcutBar {
+    /**
+     * Get all shortcuts on a given bar type
+     * @param bar a non-null shortcut bar type
+     * @return a non-null, non-leaking stream
+     */
+    Stream<PlayerShortcut> getShortcutsOf(ShortcutBarEnum bar);
+
+    /**
+     * Find a shortcut on a given bar type and a given slot
+     * @param bar a non-null shortcut bar type
+     * @param slot a valid integer slot
+     * @return a non-null option
+     */
+    Optional<PlayerShortcut> findShortcut(ShortcutBarEnum bar, int slot);
+
+    /**
+     * Add a shortcut to this bar
+     * @param shortcut a non-null shortcut
+     * @return {@code true} if it has been added, {@code false} if slot was already taken
+     */
+    boolean add(PlayerShortcut shortcut);
+
+    /**
+     * Remove a shortcut from this bar
+     * @param bar a non-null shortcut bar type
+     * @param slot a valid integer slot
+     * @return {@code true} if it has been removed, {@code false} if there was no shortcut on given slot
+     */
+    boolean remove(ShortcutBarEnum bar, int slot);
+
+    /**
+     * Swap a shortcut on this bar
+     * @param bar a non-null shortcut bar type
+     * @param from a valid integer slot
+     * @param to a valid integer slot
+     * @return {@code true} if it has been swapped
+     */
+    boolean swap(ShortcutBarEnum bar, int from, int to);
+}
