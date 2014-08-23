e21209e8fb277129ef667da8ddfbfefded76b0c3
Romain-P <skryn01@gmail.com>
2014-08-23 23:40:31 +0200

    create an empty "invite" action

--- world/world-frontend/src/main/java/org/heat/world/controllers/PlayerTradesController.java
+++ world/world-frontend/src/main/java/org/heat/world/controllers/PlayerTradesController.java
@@ -1,9 +1,11 @@
 package org.heat.world.controllers;
 
+import com.ankamagames.dofus.network.messages.game.inventory.exchanges.ExchangePlayerRequestMessage;
 import org.heat.world.players.Player;
 import org.rocket.network.Controller;
 import org.rocket.network.NetworkClient;
 import org.rocket.network.Prop;
+import org.rocket.network.Receive;
 
 import javax.inject.Inject;
 
@@ -11,4 +13,9 @@ import javax.inject.Inject;
 public class PlayerTradesController {
     @Inject NetworkClient client;
     @Inject Prop<Player> player;
+
+    @Receive
+    public void invite(ExchangePlayerRequestMessage msg) {
+
+    }
 }
