9268bf2a284c501868b65d67b8739cd42b78f678
Romain-P <skryn01@gmail.com>
2014-08-24 00:19:06 +0200

    provide a player registry

--- world-api/world-players-api/src/main/java/org/heat/world/StdPlayersModule.java
+++ world-api/world-players-api/src/main/java/org/heat/world/StdPlayersModule.java
@@ -10,7 +10,9 @@ import com.typesafe.config.Config;
 import org.heat.world.metrics.Experience;
 import org.heat.world.metrics.Experiences;
 import org.heat.world.players.DefaultPlayerFactory;
+import org.heat.world.players.DefaultPlayerRegistry;
 import org.heat.world.players.PlayerFactory;
+import org.heat.world.players.PlayerRegistry;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -30,5 +32,11 @@ public class StdPlayersModule extends AbstractModule {
     Experience providePlayerExperience(Config config) {
         Path path = Paths.get(config.getString("heat.world.experience-table-path"));
         return Experiences.read(path, Experiences.PLAYER_COLUMN);
+    }
+
+    @Provides
+    @Singleton
+    PlayerRegistry providePlayerRegistry(EventBusBuilder eventBusBuilder) {
+        return new DefaultPlayerRegistry(eventBusBuilder.build());
     }
 }
