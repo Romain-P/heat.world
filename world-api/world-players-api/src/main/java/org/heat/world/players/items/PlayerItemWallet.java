395b2553c986fb9efb6fd048526d9b64fbcf490a
Romain-P <skryn01@gmail.com>
2014-08-23 04:59:27 +0200

    create PlayerItemWallet#getEquipedStream

--- world-api/world-players-api/src/main/java/org/heat/world/players/items/PlayerItemWallet.java
+++ world-api/world-players-api/src/main/java/org/heat/world/players/items/PlayerItemWallet.java
@@ -1,11 +1,20 @@
 package org.heat.world.players.items;
 
+import org.heat.world.items.WorldItem;
 import org.heat.world.items.WorldItemWallet;
+
+import java.util.stream.Stream;
+
+import static com.ankamagames.dofus.network.enums.CharacterInventoryPositionEnum.INVENTORY_POSITION_NOT_EQUIPED;
 
 public interface PlayerItemWallet extends WorldItemWallet {
     default int getWeight() {
         return getItemStream()
                 .mapToInt(item -> (int) item.getTemplate().getRealWeight())
                 .reduce(0, Integer::sum);
+    }
+
+    default Stream<WorldItem> getEquipedStream() {
+        return getItemStream().filter(x -> x.getPosition() != INVENTORY_POSITION_NOT_EQUIPED);
     }
 }
