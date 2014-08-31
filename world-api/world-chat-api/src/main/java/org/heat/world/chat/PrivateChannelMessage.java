a944273df642806d90565ce6ab06d986f6d162ae
Romain-P <skryn01@gmail.com>
2014-08-31 22:05:15 +0200

    enhance PrivateChannelMessage

--- world-api/world-chat-api/src/main/java/org/heat/world/chat/PrivateChannelMessage.java
+++ world-api/world-chat-api/src/main/java/org/heat/world/chat/PrivateChannelMessage.java
@@ -1,24 +1,50 @@
 package org.heat.world.chat;
 
-public final class PrivateChannelMessage implements WorldChannelMessage {
-    private final String receiver;
-    private final WorldChannelMessage message;
-
-    public PrivateChannelMessage(String receiver, WorldChannelMessage message) {
-        this.receiver = receiver;
-        this.message = message;
-    }
-
-    public String getReceiver() {
-        return receiver;
-    }
-
-    public WorldChannelMessage getMessage() {
-        return message;
-    }
+public interface PrivateChannelMessage extends WorldChannelMessage {
+    WorldChannelMessage getMessage();
 
     @Override
-    public String getString() {
-        return message.getString();
+    default String getString() {
+        return getMessage().getString();
     }
+
+    static abstract class Base implements PrivateChannelMessage {
+        private final WorldChannelMessage message;
+
+        protected Base(WorldChannelMessage message) {
+            this.message = message;
+        }
+
+        @Override
+        public WorldChannelMessage getMessage() {
+            return message;
+        }
+    }
+
+    public static final class ByReceiverId extends Base {
+        private final int receiverId;
+
+        public ByReceiverId(WorldChannelMessage message, int receiverId) {
+            super(message);
+            this.receiverId = receiverId;
+        }
+
+        public int getReceiverId() {
+            return receiverId;
+        }
+    }
+
+    public static final class ByReceiverName extends Base {
+        private final String receiverName;
+
+        public ByReceiverName(WorldChannelMessage message, String receiverName) {
+            super(message);
+            this.receiverName = receiverName;
+        }
+
+        public String getReceiverName() {
+            return receiverName;
+        }
+    }
+
 }
