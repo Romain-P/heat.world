a918e1674dc41194b15683695593e6f27d927d6c
Romain-P <skryn01@gmail.com>
2014-08-31 22:27:07 +0200

    forgot to clean dedicated channel

--- world-api/world-chat-api/src/main/java/org/heat/world/chat/DedicatedWorldChannel.java
+++ world-api/world-chat-api/src/main/java/org/heat/world/chat/DedicatedWorldChannel.java
@@ -1,30 +1,22 @@
 package org.heat.world.chat;
 
 import com.github.blackrush.acara.EventBus;
-import org.heat.UserRank;
 
 import java.time.Clock;
 import java.time.Instant;
 
 public final class DedicatedWorldChannel implements VirtualWorldChannel {
     private final EventBus eventBus;
-    private final UserRank minRank;
     private final Clock clock;
 
-    public DedicatedWorldChannel(EventBus eventBus, UserRank minRank, Clock clock) {
+    public DedicatedWorldChannel(EventBus eventBus, Clock clock) {
         this.eventBus = eventBus;
-        this.minRank = minRank;
         this.clock = clock;
     }
 
     @Override
     public EventBus getEventBus() {
         return eventBus;
-    }
-
-    @Override
-    public boolean canSpeak(WorldSpeaker speaker) {
-        return minRank.enough(speaker.getSpeakerRank());
     }
 
     @Override
